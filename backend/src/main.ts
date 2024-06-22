1747bfe0442dbd78348dc53d3f53f43bf3b9bec0
VariableSan <nursultan_nurgaliyev@proton.me>
2024-06-22 19:17:24 +0500

    Added backend Dockerfile

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const appPort = configService.get('APP_PORT') ?? 3000;
+
+  await app.listen(appPort);
+
+  logger.log(`The service is running on port ${appPort}`);
 }
 bootstrap();

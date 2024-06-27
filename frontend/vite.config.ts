d773c47fb6fbb0a3fed9d1da0b6fdbd6016daafd
Nursultan Nurgaliyev <n.nurgaliyev@tekmates.pro>
2024-06-27 15:49:30 +0500

    Changed vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { version as pkgVersion } from './package.json'
 
 process.env.VITE_APP_VERSION = pkgVersion
@@ -11,36 +11,40 @@ if (process.env.NODE_ENV === 'production') {
   process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString()
 }
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-        {
-          '@/store': ['useStore', 'useGlobalStore'],
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia',
+          {
+            '@/store': ['useStore', 'useGlobalStore'],
+          },
+          unheadVueComposablesImports,
+        ],
+        dts: 'auto-imports.d.ts',
+        vueTemplate: true,
+        eslintrc: {
+          enabled: true,
         },
-        unheadVueComposablesImports,
-      ],
-      dts: 'auto-imports.d.ts',
-      vueTemplate: true,
-      eslintrc: {
-        enabled: true,
+      }),
+      Components({
+        dts: 'components.d.ts',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }),
-    Components({
-      dts: 'components.d.ts',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  preview: {
-    host: true,
-    port: Number(process.env.VITE_APP_PORT) || 4173,
-  },
-})
+    preview: {
+      host: true,
+      port: Number(process.env.VITE_APP_PORT) || 4173,
+    },
+  })
+}
